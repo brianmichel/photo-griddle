7e7c2c0071c85ada12228934b14140a41bce509d
Brian Michel <brian.michel@gmail.com>
2021-12-28 17:32:46 -0500

    Fix metadata

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,25 +1,13 @@
 <template>
   <div class="media-container">
     <img :src="'/media/' + this.photo.name + '_preview.jpg'" />
-    <div class="metadata">
-      <section>
-        <header>speed</header>
-        <p class="item">{{ this.photo.exif.shutter }}</p>
-      </section>
-      <section>
-        <header>aperture</header>
-        <p class="item">ƒ/{{ this.photo.exif.aperture }}</p>
-      </section>
-      <section>
-        <header>iso</header>
-        <p class="item">{{ this.photo.exif.iso }}</p>
-      </section>
-    </div>
+    <Metadata :exif="this.photo.exif" />
   </div>
 </template>
 
 <script lang="js">
 import { defineComponent } from "vue";
+import Metadata from "@/components/Metadata";
 
 export default defineComponent({
   name: "Detail",
@@ -29,78 +17,26 @@ export default defineComponent({
       return this.store.photoForName(this.$route.params.filename);;
     },
   },
-  components: {},
+  components: {
+    Metadata,
+  },
 });
 </script>
 
 <style scoped lang="scss">
 .media-container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-content: center;
-  align-items: flex-start;
 }
 
 img {
   height: 100%;
   width: 100%;
-  max-height: 95vh;
-  max-width: 120vh;
+  max-height: 90vh;
+  max-width: 130vh;
   object-fit: contain;
+  padding-bottom: 1vh;
 }
-
-.metadata {
-  display: flex;
-  flex-direction: column;
-  font-family: monospace;
-  background-color: #fc0;
-  border-top-right-radius: 1vh;
-  border-bottom-right-radius: 1vh;
-  -webkit-backdrop-filter: blur(5px); /* Safari 6.0 - 9.0 */
-  backdrop-filter: blur(5px);
-  padding: 5px 10px 5px 10px;
-
-  section {
-    margin-bottom: 15px;
-  }
-
-  section:last-of-type {
-    margin-bottom: 0px;
-  }
-
-  header {
-    font-family: sans-serif;
-    text-transform: uppercase;
-    font-weight: bold;
-    font-size: 1.8vh;
-    margin: 0px;
-    color: rgba(0, 0, 0, 0.2);
-  }
-
-  .item {
-    color: black;
-    margin: 0px;
-    font-weight: bold;
-    font-size: 1.9vh;
-  }
-}
-
-@media (max-aspect-ratio: 1/1) {
-  .metadata {
-    width: 100%;
-  }
-}
-
-// @media (prefers-color-scheme: dark) {
-//   .metadata {
-//     background-color: rgba(49, 49, 49, 0.308);
-//   }
-// }
-
-// @media (prefers-color-scheme: light) {
-//   .metadata {
-//     background-color: rgba(255, 255, 255, 0.308);
-//   }
-// }
 </style>
---
+++ src/components/Metadata.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="metadata">
+    <section>
+      <header>camera</header>
+      <p class="item">{{ this.exif.camera }}</p>
+    </section>
+    <section>
+      <header>speed</header>
+      <p class="item">{{ this.exif.shutter }}</p>
+    </section>
+    <section>
+      <header>aperture</header>
+      <p class="item">ƒ/{{ this.exif.aperture }}</p>
+    </section>
+    <section>
+      <header>iso</header>
+      <p class="item">{{ this.exif.iso }}</p>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Exif } from "@/models/Photo";
+
+export default defineComponent({
+  name: "Metadata",
+  props: {
+    exif: Object as PropType<Exif>,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.metadata {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-family: monospace;
+  background-color: rgba(255, 204, 0, 0.804);
+  border-radius: 1vh;
+  -webkit-backdrop-filter: blur(5px); /* Safari 6.0 - 9.0 */
+  backdrop-filter: blur(5px);
+  padding: 10px;
+
+  header {
+    font-family: sans-serif;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 1.8vh;
+    margin: 0px;
+    color: rgba(0, 0, 0, 0.2);
+  }
+
+  .item {
+    margin: 0px;
+    font-weight: bold;
+    font-size: 1.9vh;
+  }
+}
+
+// @media (prefers-color-scheme: dark) {
+//   .metadata {
+//     background-color: rgba(49, 49, 49, 0.308);
+//   }
+// }
+
+// @media (prefers-color-scheme: light) {
+//   .metadata {
+//     background-color: rgba(255, 255, 255, 0.308);
+//   }
+// }
+</style>
