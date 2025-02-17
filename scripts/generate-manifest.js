4f2b9c7f64d4d39d726f70f5c7523e0c68a18612
Brian Michel <brian.michel@gmail.com>
2025-02-16 21:02:27 -0500

    Add more parallelization

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -1,59 +1,60 @@
-const sharp = require('sharp')
-const fs = require('fs')
-const path = require('path')
-const glob = require('glob')
-const exif = require('exifreader')
-const util = require('util')
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const exif = require("exifreader");
 
-const config = JSON.parse(fs.readFileSync("./src/assets/website-config.json")).manifest_generation
+const config = JSON.parse(
+  fs.readFileSync("./src/assets/website-config.json")
+).manifest_generation;
 
 // TODO: Using ~ in the path doesn't expand, how do I fix this without another library?
-const input = path.join(config.input_directory, '*.jpg')
-const photoDestination = path.join(config.photo_output_directory)
-const manifestDestination = path.join(config.manifest_output_directory)
+const input = path.join(config.input_directory, "*.jpg");
+const photoDestination = path.join(config.photo_output_directory);
+const manifestDestination = path.join(config.manifest_output_directory);
 
 /**
  * Read selective metadata out of a photo's EXIF tag (if possible).
  *
  * @param string filename The filename of the photo to extract metadata from if possible.
- * @returns An object with keys detailing various pieces of metadata for the photo. 
+ * @returns An object with keys detailing various pieces of metadata for the photo.
  */
 async function metadata(filename) {
-  let data = {}
+  let data = {};
 
   const tags = await exif.load(filename);
-  const camera = tags['Model']
-  data.camera = '-'
+  const camera = tags["Model"];
+  data.camera = "-";
   if (camera !== undefined) {
-    data.camera = camera.description
+    data.camera = camera.description;
   }
 
-  const created = tags['DateCreated']
-  data.created = new Date().toJSON()
+  const created = tags["DateCreated"];
+  data.created = new Date().toJSON();
   if (created !== undefined) {
-    data.created = created.description
-    console.log(created.description)
+    data.created = created.description;
+    console.log(created.description);
   }
 
-  const aperture = tags['FNumber']
-  data.aperture = '-'
+  const aperture = tags["FNumber"];
+  data.aperture = "-";
   if (aperture !== undefined && aperture.value.length == 2) {
-    data.aperture = `${aperture.value[0] / aperture.value[1]}`
+    data.aperture = `${aperture.value[0] / aperture.value[1]}`;
   }
 
-  const shutter = tags['ExposureTime']
-  data.shutter = '-'
+  const shutter = tags["ExposureTime"];
+  data.shutter = "-";
   if (shutter !== undefined && shutter.value.length == 2) {
-    data.shutter = `${shutter.value[0]}/${shutter.value[1]}`
+    data.shutter = `${shutter.value[0]}/${shutter.value[1]}`;
   }
 
-  const iso = tags['ISOSpeedRatings']
-  data.iso = '-'
+  const iso = tags["ISOSpeedRatings"];
+  data.iso = "-";
   if (iso !== undefined) {
-    data.iso = `${iso.value}`
+    data.iso = `${iso.value}`;
   }
 
-  return data
+  return data;
 }
 
 /**
@@ -63,37 +64,37 @@ async function metadata(filename) {
  * @param string output The directory in which to output the resizes.
  */
 async function resize(filename, output) {
-  const filePath = path.parse(filename)
+  const filePath = path.parse(filename);
 
   const sizes = [
     {
-      name: 'preview',
+      name: "preview",
       path: path.join(output, `${filePath.name}_preview.jpg`),
-      dimension: 2000
+      dimension: 2000,
     },
     {
-      name: 'thumb',
+      name: "thumb",
       path: path.join(output, `${filePath.name}_thumb.jpg`),
-      dimension: 600
-    }
-  ]
+      dimension: 600,
+    },
+  ];
 
-
-  return Promise
-    .all(sizes.map((size) => {
+  return Promise.all(
+    sizes.map((size) => {
       return sharp(filename)
         .resize(size.dimension, null, {
           kernel: sharp.kernel.lanczos3,
-          fit: sharp.fit.outside
+          fit: sharp.fit.outside,
         })
         .toFile(size.path)
-        .then(value => {
+        .then((value) => {
           return {
             name: size.name,
-            ...value
-          }
-        })
-    }))
+            ...value,
+          };
+        });
+    })
+  );
 }
 
 /**
@@ -104,47 +105,56 @@ async function resize(filename, output) {
  */
 async function processPhoto(filename) {
   let photo = {
-    exif: {}
-  }
+    exif: {},
+  };
 
-  const filePath = path.parse(filename)
-  photo.name = filePath.name
-  photo.filename = filePath.base
-  photo.exif = await metadata(filename)
+  const filePath = path.parse(filename);
+  photo.name = filePath.name;
+  photo.filename = filePath.base;
+  photo.exif = await metadata(filename);
 
-  return photo
+  return photo;
 }
 
 /**
- *  The main entry point for the manifest generation. 
- * 
+ *  The main entry point for the manifest generation.
+ *
  * @param string inputDirectory The input directory to read the source photos from.
  * @param string photoOutput The directory in which to place the processed photos.
  * @param string manifestOutput The directory in which to place the manifest file.
- * 
+ *
  * @note This is called with the weird format to allow for it to be marked as async.
  */
 (async function generate(inputDirectory, photoOutput, manifestOutput) {
-  const files = glob.sync(inputDirectory)
-  fs.mkdirSync(photoOutput, { recursive: true })
+  const files = glob.sync(inputDirectory);
+  fs.mkdirSync(photoOutput, { recursive: true });
 
   let manifest = {
     generated: Date.now(),
     version: 1,
-    photos: []
-  }
+    photos: [],
+  };
 
-  const totalFiles = files.length
+  const totalFiles = files.length;
+  const processingPromises = [];
 
   for (const [index, file] of files.entries()) {
-    console.info(`(${index + 1} / ${totalFiles}) Processing file: ${file}...`)
-    let jsonPhoto = await processPhoto(file)
-    const resizes = await resize(file, photoOutput)
-    let mapped = resizes.map(item => ({ [item.name]: { width: item.width, height: item.height } }))
-    jsonPhoto.sizes = Object.assign({}, ...mapped)
-    manifest.photos.push(jsonPhoto)
+    console.info(`(${index + 1} / ${totalFiles}) Processing file: ${file}...`);
+    const promise = (async () => {
+      let jsonPhoto = await processPhoto(file);
+      const resizes = await resize(file, photoOutput);
+      let mapped = resizes.map((item) => ({
+        [item.name]: { width: item.width, height: item.height },
+      }));
+      jsonPhoto.sizes = Object.assign({}, ...mapped);
+      return jsonPhoto;
+    })();
+    processingPromises.push(promise);
   }
 
-  const json = JSON.stringify(manifest)
-  fs.writeFileSync(path.join(manifestOutput, 'manifest.json'), json, 'utf-8')
-})(input, photoDestination, manifestDestination)
+  const photos = await Promise.all(processingPromises);
+  manifest.photos = photos;
+
+  const json = JSON.stringify(manifest);
+  fs.writeFileSync(path.join(manifestOutput, "manifest.json"), json, "utf-8");
+})(input, photoDestination, manifestDestination);
